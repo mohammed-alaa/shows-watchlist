a55edb225b8be0b03ecd68ea64f75efbbdd12d5e
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-08 19:18:14 +0200

    Feat(temp): test db connection

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -1,8 +1,8 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 
 export * from "../../database/tables";
 
 export function useDrizzle() {
+	console.log("db", process.env.POSTGRESS_URL);
 	return drizzle(process.env.POSTGRESS_URL!);
 }
