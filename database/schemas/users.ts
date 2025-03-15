35288827f3e30da64b27c43ad41e8f2875452468
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-15 05:05:48 +0200

    Feat: add password column to users table

--- database/schemas/users.ts
+++ database/schemas/users.ts
@@ -1,4 +1,4 @@
-import { pgTable, integer, varchar, index } from "drizzle-orm/pg-core";
+import { pgTable, integer, varchar, index, text } from "drizzle-orm/pg-core";
 
 export default pgTable(
 	"users",
@@ -6,6 +6,7 @@ export default pgTable(
 		id: integer().primaryKey().generatedAlwaysAsIdentity(),
 		name: varchar({ length: 255 }).notNull(),
 		email: varchar({ length: 255 }).notNull(),
+		password: text().notNull(),
 	},
 	(table) => [index("index_email").on(table.email)],
 );
