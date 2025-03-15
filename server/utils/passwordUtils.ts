6f392603dfd6bd127fb1375a09a7c9e5438d3106
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-16 00:37:46 +0200

    Refactor: update function name

--- server/utils/passwordUtils.ts
+++ server/utils/passwordUtils.ts
@@ -4,6 +4,9 @@ export async function hashPassword(password: string): Promise<string> {
 	return await bcrypt.hash(password, Number(process.env.PASSWORD_ROUNDS));
 }
 
-export async function compare(plain: string, hashed: string): Promise<boolean> {
+export async function comparePasswords(
+	plain: string,
+	hashed: string,
+): Promise<boolean> {
 	return await bcrypt.compare(plain, hashed);
 }
