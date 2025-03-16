df27756f373144e5312e0b198103b49e708be6e6
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-16 04:18:34 +0200

    Feat: improve functions, add validate token method

--- stores/auth.ts
+++ stores/auth.ts
@@ -6,21 +6,41 @@ export const useAuthStore = defineStore("auth", () => {
 
 	const isUserLoggedIn = computed(() => !!token.value);
 
+	const { withLoading, isLoading } = useLoading();
+
+	function cleanUpCredentials() {
+		setToken(null);
+		setUser(null);
+	}
+
+	function setUser(_user: TUser | null) {
+		user.value = _user;
+	}
+
+	function setToken(_token: string | null) {
+		token.value = _token;
+		if (_token === null) {
+			localStorage.removeItem(STORAGE_TOKEN_KEY_NAME);
+		} else {
+			localStorage.setItem(STORAGE_TOKEN_KEY_NAME, _token);
+		}
+	}
+
 	async function logout() {
 		if (!isUserLoggedIn.value) {
 			return Promise.resolve();
 		}
 
-		await $fetch("/api/logout", {
-			method: "DELETE",
-			headers: {
-				Authorization: token.value!,
-			},
-		});
+		await withLoading(() =>
+			$fetch("/api/logout", {
+				method: "DELETE",
+				headers: {
+					Authorization: token.value!,
+				},
+			}),
+		);
 
-		token.value = null;
-		user.value = null;
-		localStorage.removeItem(STORAGE_TOKEN_KEY_NAME);
+		cleanUpCredentials();
 		return Promise.resolve();
 	}
 
@@ -30,25 +50,50 @@ export const useAuthStore = defineStore("auth", () => {
 			body: data,
 		});
 
-		token.value = response.authToken!;
-		user.value = response.user!;
-		localStorage.setItem(STORAGE_TOKEN_KEY_NAME, token.value);
+		setToken(response.authToken!);
+		setUser(response.user!);
 		return Promise.resolve();
 	}
 
 	function readTokenFromStorageIfExists() {
-		const _token = localStorage.getItem(STORAGE_TOKEN_KEY_NAME);
-		if (_token) {
-			token.value = _token;
+		return localStorage.getItem(STORAGE_TOKEN_KEY_NAME);
+	}
+
+	async function validate() {
+		const _token = token.value ?? readTokenFromStorageIfExists();
+
+		if (!_token) {
+			return Promise.resolve();
 		}
+		try {
+			const response = await withLoading(() =>
+				$fetch("/api/validate-token", {
+					headers: {
+						Authorization: _token,
+					},
+				}),
+			);
+			setUser(response!.user);
+			setToken(_token);
+		} catch (error) {
+			cleanUpCredentials();
+		}
+
+		return Promise.resolve();
+	}
+
+	async function init() {
+		await validate();
 	}
 
 	return {
 		user,
 		isUserLoggedIn,
+		isLoading,
 
 		logout,
 		login,
-		readTokenFromStorageIfExists,
+		init,
+		validate,
 	};
 });
