70537914078b52e67c44085937a95cdf267b61df
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-16 02:20:54 +0200

    Refactor: rename `revokeSession` functions

--- modules/session/actions/revokeSession.ts
+++ modules/session/actions/revokeSession.ts
@@ -2,7 +2,7 @@ import { eq } from "drizzle-orm";
 import { decodeSession } from "../utils/sessionTokenHandler";
 import { cleanToken } from "../utils/authTokenHandler";
 
-export async function BySession(token: string) {
+export async function bySession(token: string) {
 	const drizzle = useDrizzle();
 	const config = useRuntimeConfig();
 
@@ -17,7 +17,7 @@ export async function BySession(token: string) {
 		.where(eq(sessionsTable.session, decodedToken.sessionToken));
 }
 
-export async function ByUserId(userId: number) {
+export async function byUserId(userId: number) {
 	const drizzle = useDrizzle();
 
 	await drizzle.delete(sessionsTable).where(eq(sessionsTable.userId, userId));
