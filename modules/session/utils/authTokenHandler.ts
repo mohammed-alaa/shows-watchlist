c91b3f5f66ee9532dbf8a727497008a289515f1b
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-16 04:08:31 +0200

    Refactor: replace values with constants

--- modules/session/utils/authTokenHandler.ts
+++ modules/session/utils/authTokenHandler.ts
@@ -1,18 +1,23 @@
 import { createHmac, createSecretKey, randomBytes } from "crypto";
 
-export function createToken(secret: string, passwordRounds: number) {
-	const secretKey = createSecretKey(secret, "utf8");
-	const randomString = randomBytes(passwordRounds).toString("utf8");
+const ENCODING = "utf8";
+const TOKEN_BUILD_WORD = "Bearer ";
+const HMAC_ALGORITHM = "sha512";
+const TOKEN_ENCODING = "base64";
 
-	return createHmac("sha512", secretKey)
+export function createToken(secret: string, passwordRounds: number) {
+	const secretKey = createSecretKey(secret, ENCODING);
+	const randomString = randomBytes(passwordRounds).toString(ENCODING);
+
+	return createHmac(HMAC_ALGORITHM, secretKey)
 		.update(randomString)
-		.digest("base64");
+		.digest(TOKEN_ENCODING);
 }
 
 export function buildToken(token: string) {
-	return `Bearer ${token}`;
+	return TOKEN_BUILD_WORD + token;
 }
 
 export function cleanToken(token: string) {
-	return token.replace("Bearer ", "");
+	return token.replace(TOKEN_BUILD_WORD, "");
 }
