587c0b2effcfa9c524b0a4c45689f606c80bac57
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-16 04:18:56 +0200

    Feat: init auth on app ready event

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const auth = useAuthStore();
+
+onNuxtReady(() => auth.init());
+</script>
+
+<template>
+	<main>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</main>
+</template>
