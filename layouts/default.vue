1111f1ca97f5e7c347df4cfc3d3d2e25798a042c
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-16 02:57:55 +0200

    Feat: add default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const auth = useAuthStore();
+
+async function logout() {
+	await auth.logout();
+	navigateTo({ name: "login" });
+}
+</script>
 
 <template>
+	<nav class="flex gap-4 items-center justify-center">
+		<NuxtLink :to="{ name: 'index' }">Index</NuxtLink>
+
+		<template v-if="auth.isUserLoggedIn">
+			<button @click.prevent="logout">Logout</button>
+		</template>
+		<template v-else>
+			<NuxtLink :to="{ name: 'register' }">Register</NuxtLink>
+			<NuxtLink :to="{ name: 'login' }">Login</NuxtLink>
+		</template>
+	</nav>
 	<NuxtPage />
 </template>
