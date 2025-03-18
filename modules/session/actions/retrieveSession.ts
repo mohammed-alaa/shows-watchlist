6b2a6d2565b2b689f864f6f4d2f41066c34847ed
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-18 09:12:36 +0200

    Fix: fix file name, Feat: add new methods to get session

---
+++ modules/session/actions/retrieveSession.ts
@@ -0,0 +1,41 @@
+import { decodeSession } from "../utils/sessionTokenHandler";
+import { eq } from "drizzle-orm";
+
+export async function getByToken(token: string) {
+	const drizzle = useDrizzle();
+	const config = useRuntimeConfig();
+
+	const decodedToken = decodeSession(config.APP_KEY, token);
+
+	if (decodedToken === null) {
+		return null;
+	}
+
+	const session = await drizzle
+		.select()
+		.from(sessionsTable)
+		.where(eq(sessionsTable.session, decodedToken.sessionToken))
+		.limit(1);
+
+	if (session.length === 0) {
+		return Promise.resolve(null);
+	}
+
+	return Promise.resolve(session[0]);
+}
+
+export async function getByUserId(userId: number) {
+	const drizzle = useDrizzle();
+
+	const session = await drizzle
+		.select()
+		.from(sessionsTable)
+		.where(eq(sessionsTable.userId, userId))
+		.limit(1);
+
+	if (session.length === 0) {
+		return Promise.resolve(null);
+	}
+
+	return Promise.resolve(session[0]);
+}
