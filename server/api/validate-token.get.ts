0e7175490eb69f59b9b6a5fce6aa03f555de135c
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-18 11:23:09 +0200

    Refactor: add return type to `validate-token`endpoint

--- server/api/validate-token.get.ts
+++ server/api/validate-token.get.ts
@@ -1,17 +1,19 @@
 import authTokenValidation from "@services/auth-token-validation";
 
-export default defineEventHandler(async (event) => {
-	const { authToken } = await getUserSessionData(event);
+export default defineEventHandler(
+	async (event): Promise<undefined | { user: TUser }> => {
+		const { authToken } = await getUserSessionData(event);
 
-	if (!authToken) {
-		return;
-	}
+		if (!authToken) {
+			return;
+		}
 
-	try {
-		const user = await authTokenValidation(authToken);
-		return { user };
-	} catch (error) {
-		await clearUserSession(event);
-		setResponseStatus(event, 401);
-	}
-});
+		try {
+			const user = await authTokenValidation(authToken);
+			return { user: user.withoutPassword() };
+		} catch (error) {
+			await clearUserSession(event);
+			setResponseStatus(event, 401);
+		}
+	},
+);
