80c3561a7caf3c0ad4d082b586e62d98a12f8899
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-18 11:30:48 +0200

    Feat: add route utils, protected & public routes

--- constants.ts
+++ constants.ts
@@ -4,10 +4,10 @@ export const AUTH = {
 } as const;
 
 export const API_ROUTES = {
-	VALIDATE_TOKEN: "/api/validate-token",
-	LOGIN: "/api/login",
-	LOGOUT: "/api/logout",
-	REGISTER: "/api/register",
+	VALIDATE_TOKEN: "/validate-token",
+	LOGIN: "/login",
+	LOGOUT: "/logout",
+	REGISTER: "/register",
 } as const;
 
 export const AUTH_TOKEN_GENERATOR = {
@@ -15,3 +15,11 @@ export const AUTH_TOKEN_GENERATOR = {
 	HMAC_ALGORITHM: "sha512",
 	TOKEN_ENCODING: "base64",
 } as const;
+
+export const PROTECTED_ROUTES: string[] = ["/", API_ROUTES.LOGOUT];
+
+export const PUBLIC_ROUTES: string[] = [
+	API_ROUTES.LOGIN,
+	API_ROUTES.REGISTER,
+	API_ROUTES.VALIDATE_TOKEN,
+];
---
+++ utils/route.ts
@@ -0,0 +1,54 @@
+import { PROTECTED_ROUTES, PUBLIC_ROUTES, API_ROUTES } from "@constants";
+
+const API_START = "/api";
+
+type ApiRoute = (typeof API_ROUTES)[keyof typeof API_ROUTES];
+type TWithApiPrefix<T extends ApiRoute> = `${typeof API_START}${T}`;
+
+/**
+ * Check if a route is protected
+ * @param path The path of the route
+ * @param strict If true, the path will be checked as is, otherwise it will be checked as an API route
+ * @returns True if the route is protected
+ */
+export function isRouteProtected(
+	path: string,
+	strict: boolean = true,
+): boolean {
+	if (!strict && !isRouteApi(path)) {
+		path += API_START;
+	}
+
+	return PROTECTED_ROUTES.includes(path);
+}
+
+/**
+ * Check if a route is public
+ * @param path The path of the route
+ * @param strict If true, the path will be checked as is, otherwise it will be checked as
+ * @returns True if the route is public
+ */
+export function isRoutePublic(path: string, strict: boolean = true): boolean {
+	if (!strict && !isRouteApi(path)) {
+		path += API_START;
+	}
+	return PUBLIC_ROUTES.includes(path);
+}
+
+/**
+ * Check if a route is an API route
+ * @param path The path of the route
+ * @returns True if the route is an API route
+ */
+export function isRouteApi(path: string): boolean {
+	return path.startsWith(API_START);
+}
+
+/**
+ * Add the API prefix to a route
+ * @param path The path of the route
+ * @returns The path with the API prefix
+ */
+export function withApiPrefix<T extends ApiRoute>(path: T): TWithApiPrefix<T> {
+	return `${API_START}${path}`;
+}
