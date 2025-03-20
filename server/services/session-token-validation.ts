38d475abf5a0cc3adec55a7352ff116f6e6f89c7
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-20 05:08:53 +0200

    Feat: rename service, refactor service
    - Rename service from `auth-token-validation` to `session-token-validation`.
    - Clear session data if it's invalid.
    - Reject promise if session is invalid.

---
+++ server/services/session-token-validation.ts
@@ -0,0 +1,28 @@
+import type { H3Event } from "h3";
+import { getByToken } from "@modules/session/actions/retrieveSession";
+import User from "@modules/user/dtos/user";
+
+/**
+ * Service that gets user information by session token
+ * @param {H3Event} event - Session token
+ * @returns {User} User information
+ * @throws {Error} Error - If the session token is invalid
+ * @throws {Error} Error - If the session token is not found
+ * @throws {Error} Error - If the session token is expired
+ */
+export default async function (event: H3Event): Promise<User> {
+	const { authToken } = await getUserSessionData(event);
+
+	try {
+		if (!authToken) {
+			throw new Error();
+		}
+
+		const session = await getByToken(authToken);
+		return Promise.resolve(User.fromDb(session.users));
+	} catch (error) {
+		await clearUserSession(event);
+		setResponseStatus(event, 401);
+		return Promise.reject(null);
+	}
+}
