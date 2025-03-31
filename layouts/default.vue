10fcaf243249884baa707944099a8656cd1a48e8
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-03-31 05:29:13 +0200

    Feat: add search route to navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,12 +9,12 @@ async function logout() {
 
 <template>
 	<nav class="flex gap-4 items-center justify-center">
-		<NuxtLink :to="{ name: 'index' }">Index</NuxtLink>
-
 		<template v-if="auth.isLoading">
 			<p>Loading...</p>
 		</template>
 		<template v-else-if="auth.isUserLoggedIn">
+			<NuxtLink :to="{ name: 'index' }">Index</NuxtLink>
+			<NuxtLink :to="{ name: 'search' }">Search</NuxtLink>
 			<button @click.prevent="logout">Logout</button>
 		</template>
 		<template v-else>
