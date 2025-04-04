4b207c3f6f5db6895ac0805c2275218474cb8d73
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-04-04 16:39:02 +0200

    Feat: retrieve show details from DB

--- server/api/show-details/[type]/[id].get.ts
+++ server/api/show-details/[type]/[id].get.ts
@@ -1,26 +1,46 @@
+import { MEDIATYPES } from "@constants";
+import { byTmdbIdWithAddedLists } from "@modules/shows/actions/retrieveShows";
+import sessionTokenValidation from "@services/session-token-validation";
 import { getById } from "@services/tmdb/search";
 
-export default defineEventHandler(async (event) => {
-	const id = Number(getRouterParam(event, "id"));
-	const type = getRouterParam(event, "type") as TMediaTypes;
+export default defineEventHandler(
+	async (event): Promise<TRetrieveShowDetailsResponse> => {
+		const id = Number(getRouterParam(event, "id"));
+		const type = getRouterParam(event, "type") as TMediaTypes;
 
-	if (isNaN(id) || !(["movie", "tv"] as TMediaTypes[]).includes(type)) {
-		setResponseStatus(event, 400);
-		return {
-			errors: {
-				message: "Invalid ID or type",
-			},
-		};
-	}
+		if (isNaN(id) || !MEDIATYPES.includes(type)) {
+			setResponseStatus(event, 400);
+			return Promise.reject({
+				errors: {
+					message: "Invalid ID or type",
+				},
+			});
+		}
 
-	try {
-		return await getById(id, type);
-	} catch (error: any) {
-		setResponseStatus(event, 500);
-		return {
-			errors: {
-				message: error.message,
-			},
-		};
-	}
-});
+		try {
+			const user = await sessionTokenValidation(event);
+
+			try {
+				const details = await getById(id, type);
+				const show = await byTmdbIdWithAddedLists(
+					details.id,
+					user.getId()!,
+				);
+
+				return {
+					details,
+					show,
+				};
+			} catch (error: any) {
+				setResponseStatus(event, 500);
+				return Promise.reject({
+					errors: {
+						message: error.message,
+					},
+				});
+			}
+		} catch (error: any) {
+			return Promise.reject();
+		}
+	},
+);
