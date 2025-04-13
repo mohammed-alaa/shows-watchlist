6057c01d7df4b5b41eb5791d7b12fba1daa7338d
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-04-13 16:59:56 +0200

    Feat: add `byIdWithShows` to `retrieveLists`, Fix: fix types

--- modules/lists/actions/retrieveLists.ts
+++ modules/lists/actions/retrieveLists.ts
@@ -1,6 +1,6 @@
-import { eq } from "drizzle-orm";
+import { eq, and, getTableColumns } from "drizzle-orm";
 
-export async function byUserId(userId: number): Promise<TList[]> {
+export async function byUserId(userId: TUser["id"]): Promise<TList[]> {
 	const drizzle = useDrizzle();
 
 	return await drizzle
@@ -9,7 +9,7 @@ export async function byUserId(userId: number): Promise<TList[]> {
 		.where(eq(listsTable.userId, userId));
 }
 
-export async function byId(listId: number): Promise<TList> {
+export async function byId(listId: TList["id"]): Promise<TList> {
 	const drizzle = useDrizzle();
 
 	const results = await drizzle
@@ -23,4 +23,36 @@ export async function byId(listId: number): Promise<TList> {
 	}
 
 	return Promise.resolve(results[0]);
+}
+
+export async function byIdWithShows(
+	listId: TList["id"],
+	userId: TUser["id"],
+): Promise<TListWithShows> {
+	const drizzle = useDrizzle();
+
+	const results = await drizzle
+		.select({
+			...getTableColumns(listsTable),
+			shows: showsTable,
+		})
+		.from(listsTable)
+		.leftJoin(showsListsTable, eq(showsListsTable.listId, listsTable.id))
+		.leftJoin(showsTable, eq(showsTable.id, showsListsTable.showId))
+		.where(and(eq(listsTable.id, listId), eq(listsTable.userId, userId)));
+
+	if (results.length === 0) {
+		return Promise.reject();
+	}
+
+	const { shows: _shows, ...rest } = results[0];
+
+	const shows = results
+		.map((list) => list.shows)
+		.filter((list) => list !== null);
+
+	return Promise.resolve({
+		list: rest,
+		shows: shows,
+	});
 }
