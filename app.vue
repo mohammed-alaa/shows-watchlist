f35199ba3377670e9c19993ed6558990d4bde385
Mohamed Alaa <mohamed.alaawo@gmail.com>
2025-04-20 20:54:40 +0200

    Feat: load lists after user authenticated

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+const authStore = useAuthStore();
+const listStore = useListStore();
+await Promise.all([authStore.init(), listStore.retrieveLists()]);
 </script>
 
 <template>
